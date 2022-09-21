38d4d078f48e3064de11017cfab4f63bcee3ba13
eomeragic1 <eomeragic1@etf.unsa.ba>
2022-09-21 13:21:23 +0200

    Clone from original repo

---
+++ run_gmixup.sh
@@ -0,0 +1,6 @@
+for seed in 1314
+do
+    CUDA_VISIBLE_DEVICES=0  python -u ./src/gmixup.py --data_path . --model GIN --dataset REDDIT-BINARY \
+        --lr 0.01 --gmixup True --seed=$seed  --log_screen True --batch_size 128 --num_hidden 64 \
+        --aug_ratio 0.15 --aug_num 10  --ge USVT
+done
