2ab279157281fada3dff0199dff0561f98508e7a
Ermin Omeragic <eomeragic1@etf.unsa.ba>
2022-09-29 11:55:37 +0200

    Added other graph estimation methods

--- src/graphon_estimator.py
+++ src/graphon_estimator.py
@@ -319,3 +319,225 @@ def gw_cost(cost_s: np.ndarray, cost_t: np.ndarray, trans: np.ndarray, p_s: np.n
     """
     cost_st = node_cost_st(cost_s, cost_t, p_s, p_t)
     return cost_st - 2 * (cost_s @ trans @ cost_t.T)
+
+def largest_gap(aligned_graphs: List[np.ndarray], k: int) -> np.ndarray:
+    """
+    Estimate a graphon by a stochastic block model based n empirical degrees
+    Reference:
+    Channarond, Antoine, Jean-Jacques Daudin, and Stéphane Robin.
+    "Classification and estimation in the Stochastic Blockmodel based on the empirical degrees."
+    Electronic Journal of Statistics 6 (2012): 2574-2601.
+    :param aligned_graphs: a list of (N, N) adjacency matrices
+    :param k: the number of blocks
+    :return: a (r, r) estimation of graphon
+    """
+    aligned_graphs = graph_numpy2tensor(aligned_graphs)
+    num_graphs = aligned_graphs.size(0)
+
+    if num_graphs > 1:
+        sum_graph = torch.mean(aligned_graphs, dim=0)
+    else:
+        sum_graph = aligned_graphs[0, :, :]  # (N, N)
+
+    num_nodes = sum_graph.size(0)
+
+    # sort node degrees
+    degree = torch.sum(sum_graph, dim=1)
+    sorted_degree = degree / (num_nodes - 1)
+    idx = torch.arange(0, num_nodes)
+
+    # find num_blocks-1 largest gap of the node degrees
+    diff_degree = sorted_degree[1:] - sorted_degree[:-1]
+    _, index = torch.topk(diff_degree, k=k - 1)
+    sorted_index, _ = torch.sort(index + 1, descending=False)
+    blocks = {}
+    for b in range(k):
+        if b == 0:
+            blocks[b] = idx[0:sorted_index[b]]
+        elif b == k - 1:
+            blocks[b] = idx[sorted_index[b - 1]:num_nodes]
+        else:
+            blocks[b] = idx[sorted_index[b - 1]:sorted_index[b]]
+
+    # derive the graphon by stochastic block model
+    probability = torch.zeros(k, k)
+    graphon = torch.zeros(num_nodes, num_nodes)
+    for i in range(k):
+        for j in range(k):
+            rows = blocks[i]
+            cols = blocks[j]
+            tmp = sum_graph[rows, :]
+            tmp = tmp[:, cols]
+            probability[i, j] = torch.sum(tmp) / (rows.size(0) * cols.size(0))
+            for r in range(rows.size(0)):
+                for c in range(cols.size(0)):
+                    graphon[rows[r], cols[c]] = probability[i, j]
+    graphon = graphon.numpy()
+    return graphon
+
+def matrix_completion(aligned_graphs: List[np.ndarray], rank: int = None) -> np.ndarray:
+    """
+    Estimate the graphon by matrix completion
+    Reference:
+    Keshavan, Raghunandan H., Andrea Montanari, and Sewoong Oh.
+    "Matrix completion from a few entries."
+    IEEE transactions on information theory 56.6 (2010): 2980-2998.
+    :param aligned_graphs: a list of (N, N) adjacency matrices
+    :param rank: the rank of adjacency matrix
+    :return: graphon: the estimated graphon model
+    """
+    aligned_graphs = graph_numpy2tensor(aligned_graphs)
+    num_graphs = aligned_graphs.size(0)
+
+    if num_graphs > 1:
+        average_graph = torch.mean(aligned_graphs, dim=0)
+    else:
+        average_graph = aligned_graphs[0, :, :]  # (N, N)
+
+    num_nodes = average_graph.size(0)
+
+    # low-rank approximation via svd
+    average_graph = 2 * (average_graph - 0.5)
+    if rank is None:
+        rank = int(num_nodes / int(np.log(num_nodes)))
+        # rank = guess_rank(average_graph)
+        # print(rank)
+    u, s, v = torch.svd(average_graph)
+    graphon = (u[:, :rank] @ torch.diag(s[:rank]) @ torch.t(v[:, :rank]) + 1) / 2
+    graphon[graphon > 1] = 1
+    graphon[graphon < 0] = 0
+    graphon = graphon.numpy()
+    return graphon
+
+
+def sorted_smooth(aligned_graphs: List[np.ndarray], h: int) -> np.ndarray:
+    """
+    Estimate a graphon by a sorting and smoothing method
+    Reference:
+    S. H. Chan and E. M. Airoldi,
+    "A Consistent Histogram Estimator for Exchangeable Graph Models",
+    Proceedings of International Conference on Machine Learning, 2014.
+    :param aligned_graphs: a list of (N, N) adjacency matrices
+    :param h: the block size
+    :return: a (k, k) step function and  a (r, r) estimation of graphon
+    """
+    aligned_graphs = graph_numpy2tensor(aligned_graphs)
+    num_graphs = aligned_graphs.size(0)
+
+    if num_graphs > 1:
+        sum_graph = torch.mean(aligned_graphs, dim=0, keepdim=True).unsqueeze(0)
+    else:
+        sum_graph = aligned_graphs.unsqueeze(0)  # (1, 1, N, N)
+
+    # histogram of graph
+    kernel = torch.ones(1, 1, h, h) / (h ** 2)
+    # print(sum_graph.size(), kernel.size())
+    graphon = torch.nn.functional.conv2d(sum_graph, kernel, padding=0, stride=h, bias=None)
+    graphon = graphon[0, 0, :, :].numpy()
+    # total variation denoising
+    graphon = denoise_tv_chambolle(graphon, weight=h)
+    return graphon
+
+def estimate_blocks_directed(aligned_graphs: List[np.ndarray], threshold: float) -> np.ndarray:
+    """
+    Estimate a graphon by stochastic block approximation.
+    Reference:
+    E. M. Airoldi, T. B. Costa, and S. H. Chan,
+    "Stochastic blockmodel approximation of a graphon: Theory and consistent estimation",
+    Advances in Neural Information Processing Systems 2013.
+    :param aligned_graphs: a list of (N, N) adjacency matrices
+    :param threshold: the threshold for singular values
+    :return: graphon: the estimated (r, r) graphon model
+    """
+    aligned_graphs = graph_numpy2tensor(aligned_graphs)
+    num_graphs = aligned_graphs.size(0)
+    num_nodes = aligned_graphs.size(1)
+    aligned_graphs = aligned_graphs.permute(1, 2, 0)
+
+    num_half_graphs = int(num_graphs / 2)
+    w = 1/((num_half_graphs + 1) * (num_nodes - num_half_graphs))
+    if num_graphs > 1:
+        sum_graph = torch.sum(aligned_graphs, dim=2)
+        if num_half_graphs > 1:
+            sum_half_graph1 = torch.sum(aligned_graphs[:, :, :num_half_graphs], dim=2)
+        else:
+            sum_half_graph1 = aligned_graphs[:, :, 0]
+
+        if num_graphs - num_half_graphs > 1:
+            sum_half_graph2 = torch.sum(aligned_graphs[:, :, num_half_graphs:], dim=2)
+        else:
+            sum_half_graph2 = aligned_graphs[:, :, -1]
+    else:
+        sum_half_graph1 = aligned_graphs[:, :, 0]
+        sum_half_graph2 = aligned_graphs[:, :, 0]
+        sum_graph = aligned_graphs[:, :, 0]
+
+    pivot_idx = [0]
+    blocks = dict()
+    blocks[0] = [pivot_idx[0]]
+    not_assigned_idx = list(range(num_nodes))
+    not_assigned_idx.remove(pivot_idx[0])
+    while len(not_assigned_idx) > 0:
+        if len(not_assigned_idx) == 1:
+            i = not_assigned_idx[0]
+        else:
+            idx = np.random.permutation(len(not_assigned_idx))
+            i = not_assigned_idx[idx[0]]
+        not_assigned_idx.remove(i)
+
+        dhat = torch.zeros(len(pivot_idx))
+        for j in range(len(pivot_idx)):
+            bj = pivot_idx[j]
+
+            set_idx = list(range(num_nodes))
+            set_idx.remove(i)
+            set_idx.remove(bj)
+
+            term1 = torch.sum(w * sum_half_graph1[i, set_idx] * sum_half_graph2[i, set_idx])
+            term2 = torch.sum(w * sum_half_graph1[bj, set_idx] * sum_half_graph2[bj, set_idx])
+            term3 = torch.sum(w * sum_half_graph1[i, set_idx] * sum_half_graph2[bj, set_idx])
+            term4 = torch.sum(w * sum_half_graph1[bj, set_idx] * sum_half_graph2[i, set_idx])
+
+            term5 = torch.sum(w * sum_half_graph1[set_idx, i] * sum_half_graph2[set_idx, i])
+            term6 = torch.sum(w * sum_half_graph1[set_idx, bj] * sum_half_graph2[set_idx, bj])
+            term7 = torch.sum(w * sum_half_graph1[set_idx, i] * sum_half_graph2[set_idx, bj])
+            term8 = torch.sum(w * sum_half_graph1[set_idx, bj] * sum_half_graph2[set_idx, i])
+
+            dhat[j] = (0.5*(torch.abs(term1 + term2 - term3 - term4) +
+                            torch.abs(term5 + term6 - term7 - term8)) / len(set_idx)) ** 0.5
+
+        if dhat.size(0) == 1:
+            value = dhat[0]
+            idx = 0
+        else:
+            value, idx = torch.min(dhat, dim=0)
+            idx = idx.item()
+
+        if value < threshold:
+            blocks[idx].append(i)
+        else:
+            blocks[len(pivot_idx)] = [i]
+            pivot_idx.append(i)
+
+    num_blocks = len(blocks)
+    for key in blocks.keys():
+        tmp = np.array(blocks[key])
+        blocks[key] = torch.from_numpy(tmp).long()
+    print(num_blocks)
+
+    # # derive the graphon by stochastic block model
+    probability = torch.zeros(num_blocks, num_blocks)
+    graphon = torch.zeros(num_nodes, num_nodes)
+    for i in range(num_blocks):
+        for j in range(num_blocks):
+            rows = blocks[i]
+            cols = blocks[j]
+            tmp = sum_graph[rows, :]
+            tmp = tmp[:, cols]
+            probability[i, j] = torch.sum(tmp) / (num_graphs * rows.size(0) * cols.size(0))
+            for r in range(rows.size(0)):
+                for c in range(cols.size(0)):
+                    graphon[rows[r], cols[c]] = probability[i, j]
+
+    graphon = graphon.numpy()
+    return graphon
