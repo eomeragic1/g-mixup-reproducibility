4cf7e3d24615c86f529ac781aa07b4bd357344ff
Ermin Omeragic <eomeragic1@etf.unsa.ba>
2022-10-04 16:59:37 +0200

    Added pooling models

--- src/models.py
+++ src/models.py
@@ -111,3 +111,158 @@ class GCN(torch.nn.Module):
         x = self.lins[1](x)
 
         return F.log_softmax(x, dim=-1)
+
+class TopKNet(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(TopKNet, self).__init__()
+
+        self.conv1 = GraphConv(in_channels, hidden_channels)
+        self.pool1 = TopKPooling(hidden_channels, ratio=0.8)
+        self.conv2 = GraphConv(hidden_channels, hidden_channels)
+        self.pool2 = TopKPooling(hidden_channels, ratio=0.8)
+        self.conv3 = GraphConv(hidden_channels, hidden_channels)
+        self.pool3 = TopKPooling(hidden_channels, ratio=0.8)
+
+        self.lin1 = torch.nn.Linear(2*hidden_channels, hidden_channels)
+        self.lin2 = torch.nn.Linear(hidden_channels, hidden_channels/2)
+        self.lin3 = torch.nn.Linear(hidden_channels/2, out_channels)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        x = F.relu(self.conv1(x, edge_index))
+        x, edge_index, _, batch, _, _ = self.pool1(x, edge_index, None, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index))
+        x, edge_index, _, batch, _, _ = self.pool2(x, edge_index, None, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv3(x, edge_index))
+        x, edge_index, _, batch, _, _ = self.pool3(x, edge_index, None, batch)
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = x1 + x2 + x3
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(self.lin2(x))
+        x = F.log_softmax(self.lin3(x), dim=-1)
+
+        return x
+
+class DiffPoolBlock(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels,
+                 normalize=False, lin=True):
+        super(DiffPoolBlock, self).__init__()
+
+        self.conv1 = DenseSAGEConv(in_channels, hidden_channels, normalize)
+        self.bn1 = torch.nn.BatchNorm1d(hidden_channels)
+        self.conv2 = DenseSAGEConv(hidden_channels, hidden_channels, normalize)
+        self.bn2 = torch.nn.BatchNorm1d(hidden_channels)
+        self.conv3 = DenseSAGEConv(hidden_channels, out_channels, normalize)
+        self.bn3 = torch.nn.BatchNorm1d(out_channels)
+
+        if lin is True:
+            self.lin = torch.nn.Linear(2 * hidden_channels + out_channels,
+                                       out_channels)
+        else:
+            self.lin = None
+
+    def bn(self, i, x):
+        batch_size, num_nodes, num_channels = x.size()
+
+        x = x.view(-1, num_channels)
+        x = getattr(self, 'bn{}'.format(i))(x)
+        x = x.view(batch_size, num_nodes, num_channels)
+        return x
+
+    def forward(self, x, adj, mask=None):
+        batch_size, num_nodes, in_channels = x.size()
+
+        x0 = x
+        x1 = self.bn(1, F.relu(self.conv1(x0, adj, mask)))
+        x2 = self.bn(2, F.relu(self.conv2(x1, adj, mask)))
+        x3 = self.bn(3, F.relu(self.conv3(x2, adj, mask)))
+
+        x = torch.cat([x1, x2, x3], dim=-1)
+
+        if self.lin is not None:
+            x = F.relu(self.lin(x))
+
+        return x
+
+
+class DiffPoolNet(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, max_nodes=150):
+        super(DiffPoolNet, self).__init__()
+
+        num_nodes = ceil(0.25 * max_nodes)
+        self.gnn1_pool = DiffPoolBlock(in_channels, hidden_channels, num_nodes)
+        self.gnn1_embed = DiffPoolBlock(in_channels, hidden_channels, hidden_channels, lin=False)
+
+        num_nodes = ceil(0.25 * num_nodes)
+        self.gnn2_pool = DiffPoolBlock(3 * hidden_channels, hidden_channels, num_nodes)
+        self.gnn2_embed = DiffPoolBlock(3 * hidden_channels, hidden_channels, hidden_channels, lin=False)
+
+        self.gnn3_embed = DiffPoolBlock(3 * hidden_channels, hidden_channels, hidden_channels, lin=False)
+
+        self.lin1 = torch.nn.Linear(3 * hidden_channels, hidden_channels)
+        self.lin2 = torch.nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x, adj, batch, mask=None):
+        s = self.gnn1_pool(x, adj, mask)
+        x = self.gnn1_embed(x, adj, mask)
+
+        x, adj, l1, e1 = dense_diff_pool(x, adj, s, mask)
+
+        s = self.gnn2_pool(x, adj)
+        x = self.gnn2_embed(x, adj)
+
+        x, adj, l2, e2 = dense_diff_pool(x, adj, s)
+
+        x = self.gnn3_embed(x, adj)
+
+        x = x.mean(dim=1)
+        x = F.relu(self.lin1(x))
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1), l1 + l2, e1 + e2
+
+
+class MinCutPoolNet(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, hidden_channels=32, max_nodes=150):
+        super(MinCutPoolNet, self).__init__()
+
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        num_nodes = ceil(0.5 * max_nodes)
+        self.pool1 = Linear(hidden_channels, num_nodes)
+
+        self.conv2 = DenseGraphConv(hidden_channels, hidden_channels)
+        num_nodes = ceil(0.5 * num_nodes)
+        self.pool2 = Linear(hidden_channels, num_nodes)
+
+        self.conv3 = DenseGraphConv(hidden_channels, hidden_channels)
+
+        self.lin1 = Linear(hidden_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index, batch):
+        x = F.relu(self.conv1(x, edge_index))
+
+        x, mask = to_dense_batch(x, batch)
+        adj = to_dense_adj(edge_index, batch)
+
+        s = self.pool1(x)
+        x, adj, mc1, o1 = dense_mincut_pool(x, adj, s, mask)
+
+        x = F.relu(self.conv2(x, adj))
+        s = self.pool2(x)
+
+        x, adj, mc2, o2 = dense_mincut_pool(x, adj, s)
+
+        x = self.conv3(x, adj)
+
+        x = x.mean(dim=1)
+        x = F.relu(self.lin1(x))
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1), mc1 + mc2, o1 + o2
