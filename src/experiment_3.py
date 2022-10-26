9aaf2a2cfbf791eca8baa6d23af07b22cb15f881
Ermin Omeragic <eomeragic1@etf.unsa.ba>
2022-10-26 13:28:15 +0200

    Added 3rd experiment partial results and analysis

--- src/experiment_3.py
+++ src/experiment_3.py
@@ -236,6 +236,6 @@ if __name__ == '__main__':
     print(f'Possible combinations: {len(combination_list)}')
 
     for i, comb in enumerate(combination_list):
-        if i >= 0:
+        if i >= 229:
             run_test(i, comb['dataset_name'], comb['model'], comb['seed'], comb['aug'])
 
